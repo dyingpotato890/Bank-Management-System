f21d590f72c080eedf92716c96852632ba52c8ad
dyingpotato890 <niranjayajayan2004@gmail.com>
2024-05-31 20:35:27 +0530

    Changed Data

--- Source/Initialize Database.py
+++ Source/Initialize Database.py
@@ -4,14 +4,14 @@ import os
 
 def customer_details(cur):
     cur.execute('create table IF NOT EXISTS customer_details(CUSTOMER_ID varchar(4) primary key,ACCOUNT_NUMBER int,NAME varchar(30),GENDER char(1),AGE int,PHONE_NUMBER varchar(20),EMAIL_ID varchar(50),BALANCE int default 0)')
-    cur.execute("insert into customer_details values('C001',952090760,'Reuben Jude Cherian','M',20,'+971561442688','reubenjudecherian@gmail.com',9000)")
-    cur.execute("insert into customer_details values('C002',970439492,'Afia Nasumudeen','F',19,'+919037458082','afianasumudeen@gmail.com',6600)")
+    cur.execute("insert into customer_details values('C001',952090760,'Liam Mitchell','M',20,'+97156165182','daroya2827@jahsec.com',9000)")
+    cur.execute("insert into customer_details values('C002',970439492,'Emma Collins','F',19,'+919747159682','daroya2827@jahsec.com',6600)")
     conn.commit()
 
 def admin_details(cur):
     cur.execute('create table IF NOT EXISTS admin_details(ADMIN_ID char(4) primary key,NAME varchar(30),DESIGNATION varchar(30),JOIN_DATE date)')
-    cur.execute("insert into admin_details values('A001','Niranjay Ajayan','Owner','2022-01-02')")
-    cur.execute("insert into admin_details values('A002','Annika Das','Manager','2022-01-02')")
+    cur.execute("insert into admin_details values('A001','Ethan Bennett','Owner','2022-01-02')")
+    cur.execute("insert into admin_details values('A002','Olivia Hayes','Manager','2022-01-02')")
     conn.commit()
 
 def customer_transactions(cur):
@@ -28,7 +28,7 @@ def add_admin(cur):
     cur.execute("select * from admin_details;")
     items = cur.fetchall()
 
-    file_path = os.path.join("C:\\Users\\niran\\Documents\\VS Code\\Python\\Bank Management System", 'adminlogin.csv')
+    file_path = os.path.join("Python\\Bank Management System", 'adminlogin.csv')
 
     with open(file_path, 'a', newline = '') as f:
         w = csv.writer(f)
@@ -48,7 +48,7 @@ def add_customer(cur):
     cur.execute("select * from customer_details;")
     items = cur.fetchall()
 
-    file_path = os.path.join("C:\\Users\\niran\\Documents\\VS Code\\Python\\Bank Management System", 'customerlogin.csv')
+    file_path = os.path.join("Python\\Bank Management System", 'customerlogin.csv')
 
     with open(file_path, 'a', newline = '') as f:
         w = csv.writer(f)
